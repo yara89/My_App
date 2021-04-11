f9af8eaf77a4ab02eaf3a023f1e8531a5e635eeb
yara89 <yara3001@gmail.com>
2021-04-11 23:50:46 +0200

    Update forms.py
    attempting address autocomplete

--- tabadol/posts/forms.py
+++ tabadol/posts/forms.py
@@ -6,10 +6,10 @@ from wtforms.validators import DataRequired, InputRequired
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
-#    lng = HiddenField('lng', validators=[DataRequired()])
-#    lat = HiddenField('lat', validators=[DataRequired()])
-#    address = HiddenField('current address', validators=[DataRequired()])
+    #lng = HiddenField('lng', validators=[DataRequired()])
+    #lat = HiddenField('lat', validators=[DataRequired()])
+    addresses = TextAreaField(
+        'Where is your sevice or give away location?', validators=[DataRequired()])
     offer_type = SelectField(u'Type of Offer', choices=[
                              (1, "give"), (2, "need")])
-# choices=[(1, "give", 2, "need")], coerce=int, validators=[InputRequired()]
     submit = SubmitField('Post')
