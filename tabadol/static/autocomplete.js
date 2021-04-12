6f6fff79bf6bccfc5ab2a733dce1b3b8b64f502a
yara89 <yara3001@gmail.com>
2021-04-12 18:29:47 +0200

    Update autocomplete.js
    removed extra unnecessary git additions

--- tabadol/static/autocomplete.js
+++ tabadol/static/autocomplete.js
@@ -57,10 +57,6 @@ function initMap() {
     infowindow.open(map, marker);
   });
 
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/main
   // Sets a listener on a radio button to change the filter type on Places
   // Autocomplete.
   function setupClickListener(id, types) {
